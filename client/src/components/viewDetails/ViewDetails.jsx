a52ebf402fc94f21b0613495453a0cc167d3f99a
Pratap Tyagi <tyagipratap111@gmail.com>
2021-07-15 12:13:13 +0530

    Created View details component with static info

--- client/src/components/viewDetails/ViewDetails.jsx
+++ client/src/components/viewDetails/ViewDetails.jsx
@@ -3,7 +3,39 @@ import "./ViewDetails.css";
 
 const ViewDetails = () => {
   const { address } = useParams();
-  return <div className="viewDetails">view {address}</div>;
+  return (
+    <div className="viewDetails">
+      <div className="left">
+        <h2>Campaigns information</h2>
+        <div>
+          <div className="card">
+            <p>Total Balance</p>
+            <p>1111</p>
+          </div>
+          <div className="card second">
+            <p>Minimum Contribution</p>
+            <p>100</p>
+          </div>
+        </div>
+        <div>
+          <div className="card">
+            <p>Total requests</p>
+            <p>0</p>
+          </div>
+          <div className="card second">
+            <p>Contributers</p>
+            <p>3000</p>
+          </div>
+        </div>
+        <button>View Requests</button>
+      </div>
+      <div className="right">
+        <h2>Contribute To Campaign</h2>
+        <input type="text" value="100" />
+        <button>Contribute</button>
+      </div>
+    </div>
+  );
 };
 
 export default ViewDetails;
