3b2e35bc4a16e821f23a562c567a1e8def1a3c55
PratapTyagi <tyagipratap111@gmail.com>
2021-12-09 23:23:08 +0530

    Hardhat setup

---
+++ contracts/factory.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+import "./campaigns.sol";
+
+contract Factory {
+    address[] campaigns;
+    
+    function addCampaign(uint minimumContribution, string _imageHash, string _title, string _description) public {
+        address newCampaign = new Campaign(minimumContribution, msg.sender, _imageHash, _title, _description);
+        campaigns.push(newCampaign);
+    }
+    
+    function getCampaigns() public view returns(address[]) {
+        return campaigns;
+    }    
+}
---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.4.17",
+  network: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+};
